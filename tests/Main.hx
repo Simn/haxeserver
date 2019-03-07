01c762d03852a79c224e04a1bd355537300c06be
Simon Krajewski <simon@haxe.org>
2019-03-07 17:02:20 +0100

    use gama-rpc

--- tests/Main.hx
+++ tests/Main.hx
@@ -4,7 +4,7 @@ class Main {
 	static function main() {
 		var process = new HaxeServerProcess([]);
 		var haxeServer = new haxeserver.sync.HaxeServer(process);
-		haxeServer.setDefaultRequestArguments(["-cp", "src"]);
+		haxeServer.setDefaultRequestArguments(["-cp", "tests", "-cp", "src", "-lib", "json-rpc"]);
 		var api = new haxeserver.sync.HaxeMethods(haxeServer);
 		trace(api.protocol.initialize({}));
 		trace(api.server.readClassPaths());
